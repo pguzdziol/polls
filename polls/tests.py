ec6264ea42b47b5e545fb6bf6b2905dfe9b970ab
Paweł Guździoł <pguzdziol@gmail.com>
2018-11-18 18:38:09 +0100

    testing our new view

--- polls/tests.py
+++ polls/tests.py
@@ -36,14 +36,14 @@ class QuestionModelTests(TestCase):
 		self.assertIs(recent_question.was_published_recently(), True)
 
 
-def create_question(question_text, days)
-	"""
-	Create a question with the given `question_text` and published the
-	given number of `days` offset to now (negative for questions published
-	in the past, positive for questions that have yet to be published).
-	"""
-	time = timezone.now() + datetime.timedelta(days=days)
-	return Question.objects.create(question_text=question_text, pub_date=time)
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
